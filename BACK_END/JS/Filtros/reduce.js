9f84b0a1f53033c325a3fd99ca4b2db6a19f87cb
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-03-08 22:11:20 -0300

    feat: Curso de JS utilizando o metodo reduce() para somar valores em arrays.

---
+++ BACK_END/JS/Filtros/reduce.js
@@ -0,0 +1,23 @@
+const salaJS = [7, 8, 8, 7, 10, 6.5, 4, 10, 7];
+const salaJava = [6, 5, 8, 9, 5, 6];
+const salaPython = [7, 3.5, 8, 9.5];
+const {log} = console;
+
+function calculaMedia(listaDeNotas) {
+    // const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => {
+    //     return acumulador + nota;
+    // }, 0);
+    const somaDasNotas = listaDeNotas.reduce((acumulador, nota) => acumulador + nota, 0);
+    const media  = somaDasNotas / listaDeNotas.length;
+    return media;
+}
+
+log(calculaMedia(salaJS));
+log(calculaMedia(salaJava));
+log(calculaMedia(salaPython));
+
+const numeros = [43, 50, 65, 12]
+ 
+const soma = numeros.reduce((acum, atual) => atual + acum, 0)
+ 
+console.log(soma) //170
