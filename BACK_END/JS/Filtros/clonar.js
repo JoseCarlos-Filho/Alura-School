b55484267f049857261da609776e2b751cc54faa
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-03-08 22:08:49 -0300

    feat: Curso de JS clonando arrays utilizando spread operator.

---
+++ BACK_END/JS/Filtros/clonar.js
@@ -0,0 +1,12 @@
+const notas = [7, 7, 8, 9];
+
+// const nota = 9.75;
+// const novaNota = nota;
+// const novoArrayNotas = [...notas];
+// novoArrayNotas.push(10);
+const novoArrayNotas = [...notas, 10];
+
+// console.log(nota);
+// console.log(novaNota);
+console.log(notas);
+console.log(novoArrayNotas);
