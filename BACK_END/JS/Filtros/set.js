e65062b1be73dd48e05f30792ccca879f1f6e6eb
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-03-08 22:12:36 -0300

    feat: Curso de JS utilizando o objeto set() para eliminar elementos repetidos de um array.

---
+++ BACK_END/JS/Filtros/set.js
@@ -0,0 +1,7 @@
+const nomes = ['Ana', 'Clara', 'Maria', 'Maria', 'João', 'João', 'João', 'José', 'José'];
+
+// const nomesAtualizados = new Set(nomes);
+const listaNomesAtualizados = [...new Set(nomes)];
+console.log(nomes);
+// console.log(nomesAtualizados);
+console.log(listaNomesAtualizados);
