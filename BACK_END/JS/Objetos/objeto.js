8a05843fdff51ca9a341a0b1db45daab8e4d1c3f
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-03-25 20:42:06 -0300

    feat: Curso de JS conhecendo objetos

---
+++ BACK_END/JS/Objetos/objeto.js
@@ -0,0 +1,11 @@
+const listaCPFs = ['12312312312', '12323443545'];
+
+const infosPessoa = ['nome', 'José Silva', 'idade', 32];
+console.log(infoPessoa[1]);
+
+const objPessoa = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript'
+}
