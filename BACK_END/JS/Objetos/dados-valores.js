ad7a7f2264415653e89ee0a51e0ee7bc209e4626
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-03-26 21:32:26 -0300

    feat: Curso de JS entendendo os dados e valoes de um objeto.

---
+++ BACK_END/JS/Objetos/dados-valores.js
@@ -0,0 +1,11 @@
+const estudante = {
+    nome: 'José Silva',
+    idade: 32,
+    cpf: '12312312312',
+    turma: 'JavaScript',
+    bolsista: true,
+    telefones: ['5511999999998', '5511999999993'],
+}
+
+const {log} = console;
+log(estudante.telefones[0]);
