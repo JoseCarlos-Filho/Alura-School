8a04dec77d6ad652f494140a6a757587a67e32a2
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-07-22 21:07:23 -0300

    feat: main.js contendo o carregamento dos dados da API e chamadas de ações

--- HTML_CSS/alura-books-api-main/app/main.js
+++ HTML_CSS/alura-books-api-main/app/main.js
@@ -1,30 +1,77 @@
+import exibiListaDeLivros from "./exibiLivros.js";
+import aplicaDesconto from "./aplicaDesconto.js";
+import filtraLivros from "./filtraLivros.js";
+import ordenaPorPreco from "./filtraPorPreco.js";
+import calculaValorTotalDeLivros from "./calculaValorTotal.js";
+
 let listaDeLivros = [];
 const endpointDaApi = '/livros.json';
-let sectionLivros = document.getElementById('livros');
+
 
 async function getBuscaLivrosDaAPI() {
     const resposta = await fetch(endpointDaApi);
     listaDeLivros = await resposta.json();
-    console.table(listaDeLivros);
-    exibiListaDeLivros(listaDeLivros);
+    let livrosComDesconto = aplicaDesconto(listaDeLivros)
+    exibiListaDeLivros(livrosComDesconto);
 }
+
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach(botao => {
+    botao.addEventListener('click', () => {
+        const categoria = botao.value;
+        let livrosFiltrados = filtraLivros(listaDeLivros, categoria);
+        let valorTotalDosLivros = calculaValorTotalDeLivros(livrosFiltrados);
+        console.log(valorTotalDosLivros);
+        exibiListaDeLivros(livrosFiltrados);
+        exibiValorTotalDosLivros(valorTotalDosLivros);
+    })
+})
+
+const btnOrdenaPorPreco = document.getElementById('btnOrdenarPorPreco');
+btnOrdenaPorPreco.addEventListener("click", () => {
+    let livrosOrdenadosPorPreco = ordenaPorPreco(listaDeLivros);
+    exibiListaDeLivros(livrosOrdenadosPorPreco);
+})
+
+
+// const btnLivrosDisponiveis = document.getElementById("btnLivrosDisponiveis");
+// btnLivrosDisponiveis.addEventListener("click", () => {
+//     let livroDisponivel = filtraLivros(listaDeLivros, categoria);
+// })
+
+
 
 getBuscaLivrosDaAPI();
 
-function exibiListaDeLivros(listaDeLivros) {
-    listaDeLivros.forEach(livro => {
-        sectionLivros.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-            <h2 class="livro__titulo">
-                ${livro.titulo}
-            </h2>
-            <p class="livro__descricao">${livro.autor}</p>
-            <p class="livro__preco" id="preco">${livro.preco.toFixed(2)}</p>
-            <div class="tags">
-                <span class="tag">${livro.categoria}</span>
-            </div>
+
+function exibiValorTotalDosLivros(valorTotalDosLivros) {
+    const elementoValorTotalLivrosDisponiveis = document.getElementById("valor_total_livros_disponiveis");
+    elementoValorTotalLivrosDisponiveis.innerHTML = `
+        <div class="livros__disponiveis">
+            <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotalDosLivros.toFixed(2)}</span></p>
         </div>
-        ` 
-    })
+    `
 }
+// Outra forma de solução.
+// document.querySelectorAll('.btn-categoria').forEach(btn => {
+//     btn.addEventListener('click', function() {
+//         const categoria = this.getAttribute('data-categoria');
+//         const livrosFiltrados = livros.filter(livro => livro.categoria === categoria);
+//         exibirLivros(livrosFiltrados);
+//     });
+// });
+
+// function exibirLivros(livros) {
+//     // Sua lógica para exibir os livros na tela
+// }
+
+// function filtrarELimpar(livrosFiltrados) {
+//     elementoParaInserirLivros.innerHTML = ''; // Limpa o conteúdo anterior
+//     exibirOsLivrosNaTela(livrosFiltrados); // Exibe os novos livros filtrados
+// }
+
+// // Supondo que essa função seja chamada quando um botão de categoria é clicado
+// function aoClicarNoBotao(categoria) {
+//     const livrosFiltrados = livros.filter(livro => livro.categoria === categoria);
+//     filtrarELimpar(livrosFiltrados);
+// }
