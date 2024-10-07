6ca569b1b0b3b80aee51430010dc2ba6a96df85c
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-10-07 20:01:53 -0300

    feat: adicionando classes de estilos nos botões com classList()

--- HTML_CSS/Fokus/js/main.js
+++ HTML_CSS/Fokus/js/main.js
@@ -12,6 +12,10 @@ const displayTitle = document.querySelector('.app__title');
 const displayImagem = document.querySelector('.app__image');
 const btnStartPause = document.getElementById('start-pause');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title');
+const botoes = document.querySelectorAll('.app__card-button');
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('/sons/luna-rise-part-one.mp3');
 
 const tempFoco = 1500;
 const tempCurto = 300;
@@ -24,19 +28,41 @@ const tempLongo = 900;
 
 btnFoco.addEventListener("click", () => {
     alterarContexto('foco');
+    btnFoco.classList.add("active");
 });
 
 btnCurto.addEventListener("click", () => {
     alterarContexto('descanso-curto');
+    btnCurto.classList.add("active");
 })
 
 btnLongo.addEventListener("click", () => {
     alterarContexto('descanso-longo');
+    btnLongo.classList.add("active");
 })
 
 function alterarContexto(contexto) {
+    botoes.forEach(contexto => {
+        contexto.classList.remove("active");
+    })
     html.setAttribute("data-contexto", contexto);
     banner.setAttribute("src", `/imagens/${contexto}.png`);
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>`;
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `Que tal dar uma respirada?<br>
+                <strong class="app__title-strong">Faça uma pausa curta!</strong>`;
+            break;
+        case "descanso-longo":
+            titulo.innerHTML = `Hora de voltar à superfície.<br>
+                <strong class="app__title-strong">Faça uma pausa longa.</strong>`;
+            break;
+        default:
+            break;
+    }
 }
 
 
