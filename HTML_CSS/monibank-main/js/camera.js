97eadc9d9941210bcccb3c880eda93df394f834a
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-09 16:35:46 -0300

    feat: adicionando imagem ao objeto com os dados de cadastro com um novo atributo imagem e salvando no navegador com localStorage.

--- HTML_CSS/monibank-main/js/camera.js
+++ HTML_CSS/monibank-main/js/camera.js
@@ -26,3 +26,13 @@ btnTirarFoto.addEventListener("click", function() {
     campoCamera.style.display = "none";
     mensagem.style.display = "block";
 })
+
+btnEnviarFoto.addEventListener("click", () => {
+    const receberDadosExistentes = localStorage.getItem("cadastro");
+    const converteRetorno = JSON.parse(receberDadosExistentes);
+
+    converteRetorno.imagem = imagemURL;
+
+    localStorage.setItem("cadastro", JSON.stringify(converteRetorno));
+    window.location.href = "./abrir-conta-form-3.html";
+})  
