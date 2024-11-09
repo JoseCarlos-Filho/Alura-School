46ecc8bfd716cd28edb44cd83733351e47f5e07e
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-09 16:00:16 -0300

    feat: adicionando evento no botão tirar foto para captura de imagem da camera.

--- HTML_CSS/monibank-main/js/camera.js
+++ HTML_CSS/monibank-main/js/camera.js
@@ -3,6 +3,10 @@ const campoCamera = document.querySelector("[data-camera]");
 const video = document.querySelector("[data-video]");
 const btnTirarFoto = document.querySelector("[data-tirar-foto]");
 const canvas = document.querySelector("[data-video-canvas]");
+const mensagem = document.querySelector("[data-mensagem]");
+const btnEnviarFoto = document.querySelector("[data-enviar]");
+
+let imagemURL = "";
 
 btnIniciarCamera.addEventListener("click", async function() {
     const iniciarVideo = await navigator.mediaDevices
@@ -12,4 +16,13 @@ btnIniciarCamera.addEventListener("click", async function() {
     campoCamera.style.display = "block";
 
     video.srcObject = iniciarVideo;
+})
+
+btnTirarFoto.addEventListener("click", function() {
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+    
+    imagemURL = canvas.toDataURL("image/jpeg");
+
+    campoCamera.style.display = "none";
+    mensagem.style.display = "block";
 })
