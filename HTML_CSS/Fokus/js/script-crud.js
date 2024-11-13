dcf39fc29d123968279acfc3e9999733610dfb2b
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-11-12 21:32:49 -0300

    feat: adicionando funcionalidade de remoção da tarefa concluida.

--- HTML_CSS/Fokus/js/script-crud.js
+++ HTML_CSS/Fokus/js/script-crud.js
@@ -6,6 +6,8 @@ const ulTarefas = document.querySelector(".app__section-task-list");
 const btnCancelaTarefa = document.querySelector(".app__form-footer__button--cancel");
 const paragrafoDescricaoTarefa = document.querySelector(".app__section-active-task-description");
 
+const btnRemoverConcluidas = document.querySelector("#btn-remover-concluidas");
+
 let tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 let tarefaSelecionada = null;
 let liTarefaSelecionada = null;
@@ -63,22 +65,29 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botaoEditar);
-    // evento de click na lista de tarefas. Para selecionar uma tarefa.
-    li.onclick = () => {
-        document.querySelectorAll(".app__section-task-list-item-active").forEach(elemento => {
-            elemento.classList.remove("app__section-task-list-item-active");
-        })
-        if(tarefaSelecionada == tarefa) {
-            paragrafoDescricaoTarefa.textContent = "";
-            tarefaSelecionada = null;
-            liTarefaSelecionada = null;
-            return;
+
+    if(tarefa.completa) {
+        liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+        botaoEditar.setAttribute("disabled", 'disabled');
+    } else {
+        // evento de click na lista de tarefas. Para selecionar uma tarefa.
+        li.onclick = () => {
+            document.querySelectorAll(".app__section-task-list-item-active").forEach(elemento => {
+                elemento.classList.remove("app__section-task-list-item-active");
+            })
+            if(tarefaSelecionada == tarefa) {
+                paragrafoDescricaoTarefa.textContent = "";
+                tarefaSelecionada = null;
+                liTarefaSelecionada = null;
+                return;
+            }
+            tarefaSelecionada = tarefa;
+            liTarefaSelecionada = li;
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+            li.classList.add("app__section-task-list-item-active");
         }
-        tarefaSelecionada = tarefa;
-        liTarefaSelecionada = li;
-        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
-        li.classList.add("app__section-task-list-item-active");
     }
+
 
     return li;
 }
@@ -114,5 +123,17 @@ document.addEventListener("FocoFinalizado", () => {
         liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
         liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
         liTarefaSelecionada.querySelector("button").setAttribute("disabled", 'disabled');
+        tarefaSelecionada.completa = true;
+        atualizarTarefas();
     }
 })
+
+// botão com ações de remoção das tarefas concluidas.
+btnRemoverConcluidas.onclick = () => {
+    const seletor = ".app__section-task-list-item-complete"
+    document.querySelectorAll(seletor).forEach(elemento => {
+        elemento.remove()
+    });
+    tarefas = tarefas.filter(tarefa => !tarefa.completa);
+    atualizarTarefas();
+}
