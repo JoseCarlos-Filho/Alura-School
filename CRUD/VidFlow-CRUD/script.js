721607a6073232f34cdd5a263153ea7325b599f9
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-02 15:48:13 -0300

    feat: implementando o conceito de função assincrona(async/await) para resolver o problema de callback hell(muitas intruções de callback no caso then()).

--- CRUD/VidFlow-CRUD/script.js
+++ CRUD/VidFlow-CRUD/script.js
@@ -1,8 +1,9 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => res.json())
-.then((listaVideos) => listaVideos.forEach(video => {
+async function buscarEMostarVideos() {
+    const busca = await fetch("http://localhost:3000/videos");
+    const listaVideos = await busca.json();
+    listaVideos.forEach(video => {
     containerVideos.innerHTML += `
         <li class="videos__item">
             <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -12,8 +13,8 @@ const api = fetch("http://localhost:3000/videos")
                 <p class="titulo-canal">${video.descricao}</p>
             </div>
         </li>
-    `
-}))
-.catch(error => {
-    containerVideos.innerHTML = `<p style="color: red;">Houve um erro ao carregar os videos: ${error}</p>`
+    `;
 })
+}
+
+buscarEMostarVideos();
