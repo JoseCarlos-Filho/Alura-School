7be3dd8bcf238ec2eb50cce63390d043faa4f13f
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-07 11:10:43 -0300

    feat: Entendendo o uso do Promisse.all

--- CRUD/js-consumindo-dados-api-main/script.js
+++ CRUD/js-consumindo-dados-api-main/script.js
@@ -1,15 +1,19 @@
-async function buscaCEP () {
+async function buscaCEP (cep) {
     try {
-        const consultaCEP = await fetch("https://viacep.com.br/ws/01001000/json/");
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const resultadoCEP = await consultaCEP.json();
         if(resultadoCEP.erro) {
             throw Error("Cep não existente!");
         }
         console.log(resultadoCEP);
+        return resultadoCEP;
     } catch(erro) {
         console.log(erro);
     }
 }
 
-buscaCEP()
+let ceps = ['04168040', '01001001'];
+let conjuntoCeps = ceps.map(cep => buscaCEP(cep));
+Promise.all(conjuntoCeps).then(res => console.log(res));
+// buscaCEP()
 
