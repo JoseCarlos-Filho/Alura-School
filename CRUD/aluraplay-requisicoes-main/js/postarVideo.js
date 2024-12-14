45b72a8e6048edc298ad9b55f5c20974b078e29e
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-14 17:10:25 -0300

    feat: Tratamento de erro com bloco try/catch envia erro ao usuário caso o mesmo não consiga fazer cadastro do novo video.

--- CRUD/aluraplay-requisicoes-main/js/postarVideo.js
+++ CRUD/aluraplay-requisicoes-main/js/postarVideo.js
@@ -9,10 +9,13 @@ async function postarVideo(evento) {
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 10).toString();
+    try {
+        await conectaApi.postaVideo(titulo, descricao, url, imagem);
 
-    await conectaApi.postaVideo(titulo, descricao, url, imagem);
-
-    window.location.href = "../pages/envio-concluido.html";
+        window.location.href = "../pages/envio-concluido.html";
+    } catch(e) {
+        alert(e);
+    }
 }
 
 formulario.addEventListener("submit", evento => postarVideo(evento));
