775b637d423ab7ede20798a8f08f91c16fca2d77
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-22 21:58:53 -0300

    feat: import da classe negociações, declaração de variavel privada negociacoes e adição de negociacao na lista de negociacoes

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/controllers/negociacao-controller.ts
@@ -1,8 +1,11 @@
 import { Negociacao } from './../models/negociacao.js';
+import { Negociacoes } from '../models/negociacoes.js';
+
 export class NegociacaoController {
     private inputData: HTMLInputElement;
     private inputQuantidade: HTMLInputElement;
     private inputValor: HTMLInputElement;
+    private negociacoes = new Negociacoes();
 
     constructor() {
         this.inputData = document.querySelector("#data");
@@ -12,7 +15,9 @@ export class NegociacaoController {
 
     adiciona(): void {
         const negociacao = this.criaNegociacao();
-        console.log(negociacao);
+        this.negociacoes.adiciona(negociacao);
+        console.log(this.negociacoes.lista());
+        // console.log(negociacao);
         this.limparFormulario();
         // console.log(this.inputData.value);
         // console.log(this.inputQuantidade.value);
