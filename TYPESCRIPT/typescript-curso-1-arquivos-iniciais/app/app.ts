23c4d46d28bcc44f33c210944a707fd2d151206a
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-27 15:35:59 -0300

    feat: adicionando commendário na importação.

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/app.ts
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/app.ts
@@ -1,5 +1,4 @@
 // import { Negociacao } from "./models/negociacao.js";
-
 import { NegociacaoController } from "./controllers/negociacao-controller.js";
 
 // const negociacao = new Negociacao(new Date(), 10, 100);
@@ -11,4 +10,5 @@ const form = document.querySelector('.form');
 form.addEventListener("submit", event => {
     event.preventDefault();
     controller.adiciona();
-})
+});
+
