21f12136acc8bcfa9bb5a801e1fdf6f6e7377901
JoseCarlos-Filho <etijcvivi@gmail.com>
2024-12-27 15:43:29 -0300

    feat: classe abstrata que será herdada por outras classes que irão implementar o template. A classe usa generics para definir o tipo de dado que será passado para o template. A classe possui um método abstrato template que será implementado pelas classes filhas

---
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/views/view.ts
@@ -0,0 +1,23 @@
+// classe abstrata que será herdada por outras classes que irão implementar o template
+// A classe usa generics para definir o tipo de dado que será passado para o template
+// A classe possui um método abstrato template que será implementado pelas classes filhas
+export abstract class View<T> {
+    // protegido para que as classes filhas possam acessar o elemento
+    // somente as classes filhas podem acessar o elemento
+    protected elemento: HTMLElement;
+
+    constructor(seletor: string) {
+        this.elemento = document.querySelector(seletor);
+    }
+    // metodo abstrato onde a responsabilidade de implementar o template é da classe filha.
+
+    abstract template(model: T): string;
+    // {
+    //     throw Error('Classe filha precisa implementar o método template');
+    // }
+
+    update(model: T): void {
+        const template = this.template(model);
+        this.elemento.innerHTML = template;
+    }
+}
