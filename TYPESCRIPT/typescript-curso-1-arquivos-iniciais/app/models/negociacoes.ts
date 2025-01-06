05e6054a0c3537284f5d33189a2b1bb0b9ff52ac
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-06 17:47:43 -0300

    feat: metodo adiciona que passa a negociação como parâmentro e adiciona na lista de negociações. metodo lista() de negociações

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -4,13 +4,13 @@ export class Negociacoes {
     // essa declatação de tipo: Array<Negociacao> é o mesmo que Negociacao[]
     private negociacoes: Negociacao[] = [];
 
-    adiciona(negociacao: Negociacao) {
+    public adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
     // A declaração de tipo ReadonlyArray<Negociacao> é o mesmo que readonly Negociacao[]
     // A diferença é que ReadonlyArray é uma interface e readonly é um modificador de acesso
     // ReadonlyArray é uma interface que define um array somente leitura
-    lista(): readonly Negociacao[] {
+    public lista(): readonly Negociacao[] {
         return this.negociacoes;
     }
 }
