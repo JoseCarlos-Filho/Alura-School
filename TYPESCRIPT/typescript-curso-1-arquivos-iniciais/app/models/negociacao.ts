18fdd14bd6e9582940eb03ee32cd308a51580759
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-06 17:44:49 -0300

    feat: isolando a lógica de conversão de entrada

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/models/negociacao.ts
@@ -22,4 +22,12 @@ export class Negociacao {
     get volume(): number {
         return this.quantidade * this.valor;
     }
+
+    public criaDe(dateString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const data = new Date(dateString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseFloat(valorString);
+        return new Negociacao(data, quantidade, valor);
+    }
 }
