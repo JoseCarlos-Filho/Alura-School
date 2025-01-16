a396fba117884ed916289e2c9bc7955f3d4aa222
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-16 20:02:30 -0300

    feat: adicionando as propriedades "removeComments" e "strictNullChecks" no arquivo tsconfig para remover os comentários no JS compilado.

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/dist/js/models/negociacoes.js
@@ -1,14 +1,10 @@
 export class Negociacoes {
     constructor() {
-        // essa declatação de tipo: Array<Negociacao> é o mesmo que Negociacao[]
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
-    // A declaração de tipo ReadonlyArray<Negociacao> é o mesmo que readonly Negociacao[]
-    // A diferença é que ReadonlyArray é uma interface e readonly é um modificador de acesso
-    // ReadonlyArray é uma interface que define um array somente leitura
     lista() {
         return this.negociacoes;
     }
