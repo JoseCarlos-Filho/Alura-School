474aca133917c0ef374ca3327329f6961f91a237
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-16 20:02:45 -0300

    feat: adicionando as propriedades "removeComments" e "strictNullChecks" no arquivo tsconfig para remover os comentários no JS compilado.

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
@@ -1,6 +1,3 @@
-// classe abstrata que será herdada por outras classes que irão implementar o template
-// A classe usa generics para definir o tipo de dado que será passado para o template
-// A classe possui um método abstrato template que será implementado pelas classes filhas
 export class View {
     constructor(seletor, escapar) {
         this.escapar = false;
@@ -9,9 +6,6 @@ export class View {
             this.escapar = escapar;
         }
     }
-    // {
-    //     throw Error('Classe filha precisa implementar o método template');
-    // }
     update(model) {
         let template = this.template(model);
         if (this.escapar) {
