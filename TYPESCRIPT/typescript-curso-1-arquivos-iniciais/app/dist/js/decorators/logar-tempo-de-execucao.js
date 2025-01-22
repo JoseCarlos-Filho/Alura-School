04af91aafac0cf51804e336539a5ef7ce82a1ab1
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-01-21 22:52:24 -0300

    feat: código JS gerado pelo compilador TS configurado no arquivo tsconfig

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/dist/js/decorators/logar-tempo-de-execucao.js
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/dist/js/decorators/logar-tempo-de-execucao.js
@@ -1,14 +1,20 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos = false) {
     return function (target, propertyKey, descriptor) {
         const metodoOriginal = descriptor.value;
         descriptor.value = function (...args) {
+            let divisor = 1;
+            let unidade = 'milisegundos';
+            if (emSegundos) {
+                divisor = 1000;
+                unidade = 'segundos';
+            }
             console.log('-----------------------');
             console.log(`Parâmetros do método ${propertyKey}: ${JSON.stringify(args)}`);
             const t1 = performance.now();
             const retorno = metodoOriginal.apply(this, args);
             const t2 = performance.now();
             console.log(`O retorno do método ${propertyKey} é ${JSON.stringify(retorno)}`);
-            console.log(`O método ${propertyKey} demorou ${(t2 - t1) / 1000} segundos`);
+            console.log(`O método ${propertyKey} demorou ${(t2 - t1) / divisor} ${unidade}`);
             return retorno;
         };
         return descriptor;
