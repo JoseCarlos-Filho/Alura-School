c499edf20f13275a2018bc3e54f0bee59512d431
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-02-02 23:05:05 -0300

    feat: JS gerado pelo compilador TS faz verificação de negociação existente ou não na lista de negociacao importadas

--- TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/dist/js/models/negociacao.js
+++ TYPESCRIPT/typescript-curso-1-arquivos-iniciais/app/dist/js/models/negociacao.js
@@ -26,4 +26,9 @@ export class Negociacao {
             Volume: ${this.volume}
             `;
     }
+    ehIgual(negociacao) {
+        return this.data.getDate() === negociacao.data.getDate() &&
+            this.data.getMonth() === negociacao.data.getMonth() &&
+            this.data.getFullYear() === negociacao.data.getFullYear();
+    }
 }
