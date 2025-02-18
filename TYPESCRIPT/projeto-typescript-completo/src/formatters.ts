e7d07d4b0c6a1b39e45f1e9524bf13ced9ca73be
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-02-17 21:17:32 -0300

    feat: adiciona suporte a múltiplos formatos de data na função de formatação

--- TYPESCRIPT/projeto-typescript-completo/src/formatters.ts
+++ TYPESCRIPT/projeto-typescript-completo/src/formatters.ts
@@ -5,11 +5,27 @@ function formatarMoeda(valor: number): string {
   });
 }
 
-function formatarData(data: Date): string {
-  return data.toLocaleString("pt-BR", {
-    weekday: "long",
-    day: "2-digit",
-    month: "2-digit",
-    year: "numeric"
-  });
+function formatarData(data: Date, formato: FormatoData = FormatoData.PADRAO): string {
+    if (formato === FormatoData.DIA_SEMANA_DIA_MES_ANO) {
+        return data.toLocaleString("pt-BR", {
+            weekday: "long",
+            day: "2-digit",
+            month: "2-digit",
+            year: "numeric"
+          });
+    } else if (formato === FormatoData.DIA_MES) {
+        return data.toLocaleString("pt-BR", {
+            day: "2-digit",
+            month: "2-digit"
+          });
+    }
+
+    return data.toLocaleDateString("pt-BR");
+  
+}
+
+function formatarInformacoes(valor: number, data: Date, formatoData: FormatoData): string {
+    const dataFormatada = formatarData(data, formatoData);
+    const valorFormatado = formatarMoeda(valor);
+    return `${dataFormatada} - ${valorFormatado}`;
 }
