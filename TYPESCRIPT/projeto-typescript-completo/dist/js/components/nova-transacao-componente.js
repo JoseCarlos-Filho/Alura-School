ebf893c486b58313256b651a34c06042384f88fb
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-02-21 22:59:56 -0300

    feat: adiciona tratamento de erro e atualização do componente Extrato ao registrar nova transação

--- TYPESCRIPT/projeto-typescript-completo/dist/js/components/nova-transacao-componente.js
+++ TYPESCRIPT/projeto-typescript-completo/dist/js/components/nova-transacao-componente.js
@@ -1,23 +1,30 @@
 import SaldoComponent from "./saldo-componente.js";
 import Conta from "../types/Contas.js";
+import ExtratoComponent from "./extrato-component.js";
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (!elementoFormulario.checkValidity()) {
-        alert("Preencha todos os campos da transação!");
+    try {
+        event.preventDefault();
+        if (!elementoFormulario.checkValidity()) {
+            alert("Preencha todos os campos da transação!");
+        }
+        const inputTipoTransacao = document.querySelector("#tipoTransacao");
+        const inputValor = document.querySelector("#valor");
+        const inputData = document.querySelector("#data");
+        let tipoTransacao = inputTipoTransacao.value;
+        let valor = inputValor.valueAsNumber;
+        let data = new Date(inputData.value + " 00:00:00");
+        const novaTransacao = {
+            tipoTransacao: tipoTransacao,
+            valor: valor,
+            data: data
+        };
+        Conta.registrarTransacao(novaTransacao);
+        SaldoComponent.atualizar();
+        ExtratoComponent.atualizar();
+        elementoFormulario.reset();
     }
-    const inputTipoTransacao = document.querySelector("#tipoTransacao");
-    const inputValor = document.querySelector("#valor");
-    const inputData = document.querySelector("#data");
-    let tipoTransacao = inputTipoTransacao.value;
-    let valor = inputValor.valueAsNumber;
-    let data = new Date(inputData.value);
-    const novaTransacao = {
-        tipoTransacao: tipoTransacao,
-        valor: valor,
-        data: data
-    };
-    Conta.registrarTransacao(novaTransacao);
-    SaldoComponent.atualizar();
-    elementoFormulario.reset();
+    catch (error) {
+        alert(error.message);
+    }
 });
