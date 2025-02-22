6253f4f6e50cfe3b10152ae2ec238e74bf827881
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-02-21 23:00:45 -0300

    feat: implementa funções de debitar e depositar, além de persistência de saldo e transações

--- TYPESCRIPT/projeto-typescript-completo/src/types/Contas.ts
+++ TYPESCRIPT/projeto-typescript-completo/src/types/Contas.ts
@@ -1,7 +1,33 @@
 import { Transacao } from './Transacao.js';
 import { TipoTransacao } from './TipoTransacao.js';
+import { GrupoTransacao } from './GrupoTransacao.js';
 
-let saldo: number = 3000;
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+const transacoes: Transacao[] = localStorage.getItem("transacoes") ? JSON.parse(localStorage.getItem("transacoes"), (key: string, value: string) => {
+    if (key === "data") {
+        return new Date(value);
+    }
+    return value;
+}) : [];
+
+function debitar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("Valor a ser debitado deve ser maior que zero!");
+    }
+    if (valor > saldo) {
+        throw new Error("Saldo insuficiente!");
+    }
+    saldo -= valor;
+    localStorage.setItem("saldo", saldo.toString());
+}
+
+function depositar(valor: number): void {
+    if (valor <= 0) {
+        throw new Error("Valor a ser depositado deve ser maior que zero!");
+    }
+    saldo += valor;
+    localStorage.setItem("saldo", saldo.toString());
+}
 
 const Conta = {
     getSaldo() {
@@ -12,19 +38,49 @@ const Conta = {
         return new Date();
     },
 
+    getGruposTransacoes(): GrupoTransacao[] {
+        const gruposTransacoes: GrupoTransacao[] = [];
+        const listaTransacoes: Transacao[] = structuredClone(transacoes);
+        const transacoesOrdenadas: Transacao[] = 
+            listaTransacoes.sort((a, b) => b.data.getTime() - a.data.getTime());
+        let labelAtualGrupoTransacao: string = "";
+
+        for (let transacao of transacoesOrdenadas) {
+            let labelGrupoTransacao: string = transacao.data.toLocaleDateString("pt-BR", {
+                month: "long",
+                year: "numeric"
+            });
+            if (labelAtualGrupoTransacao !== labelGrupoTransacao) {
+                labelAtualGrupoTransacao = labelGrupoTransacao;
+                gruposTransacoes.push({
+                    label: labelGrupoTransacao,
+                    transacoes: []
+                });
+            }
+            gruposTransacoes.at(-1).transacoes.push(transacao);
+        }
+        return gruposTransacoes;
+    },
+
     registrarTransacao(novaTransacao: Transacao): void {
         if (novaTransacao.tipoTransacao === TipoTransacao.DEPOSITO) {
-                saldo += novaTransacao.valor;
-                console.log(saldo);
+                depositar(novaTransacao.valor);
+                // saldo += novaTransacao.valor;
+                // console.log(saldo);
             } else if (
                 novaTransacao.tipoTransacao === TipoTransacao.TRANSFERENCIA || 
                 novaTransacao.tipoTransacao === TipoTransacao.PAGAMENTO_BOLETO
             ) {
-                saldo -= novaTransacao.valor;
+                debitar(novaTransacao.valor);
+                novaTransacao.valor *= -1;
+                // saldo -= novaTransacao.valor;
             } else {
-                alert("Tipo de transação inválido!");
+                throw new Error("Tipo de transação inválido!");
+                // alert("Tipo de transação inválido!");
             }
-            console.log(novaTransacao);
+            transacoes.push(novaTransacao);
+            console.log(this.getGruposTransacoes());
+            localStorage.setItem("transacoes", JSON.stringify(transacoes));
     }
 }
 
