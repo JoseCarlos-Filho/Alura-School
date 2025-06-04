9add516cb39cf88b1132f5812fc9d91dfd1e3f1c
JoseCarlos-Filho <etijcvivi@gmail.com>
2025-06-03 21:42:26 -0300

    feat: verifica se a lista está vazia.

---
+++ HTML_CSS/lista-de-compras/scripts/verificarListaVazia.js
@@ -0,0 +1,13 @@
+const msgListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia(listaDeCompras) {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+
+    if (itensDaLista.length === 0) {
+        msgListaVazia.style.display = "block";
+    } else {
+        msgListaVazia.style.display = "none";
+    }
+}
+
+export default verificarListaVazia;
